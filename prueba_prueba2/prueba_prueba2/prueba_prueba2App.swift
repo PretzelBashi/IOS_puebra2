997740707871d91dae787b4f75eca6d2ff40a1cb
alainacosta64@gmail.com <diegoortega2552@gmail.com>
2026-02-09 08:41:27 -0700

    [AGREGA] Codigo Xcode

---
+++ prueba_prueba2/prueba_prueba2/prueba_prueba2App.swift
@@ -0,0 +1,17 @@
+//
+//  prueba_prueba2App.swift
+//  prueba_prueba2
+//
+//  Created by alumno on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct prueba_prueba2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
